ced5885fce47a228612e35e5243e27885e6de571
tema-skakun <frederico_f@mail.ru>
2024-08-05 00:18:03 +0300

    feat: adjusts margins on device page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { TextField, Button } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { fetchDevices, fetchDevicesById } from '../store/slices/deviceSlice';
+import React, {useState} from 'react';
+import {TextField, Button, Grid, Paper} from '@mui/material';
+import {useDispatch} from 'react-redux';
+import {fetchDevices, fetchDevicesById} from '../store/slices/deviceSlice';
 
 const SearchBar: React.FC = () => {
   const [search, setSearch] = useState('');
@@ -17,14 +17,29 @@ const SearchBar: React.FC = () => {
   };
 
   return (
-    <div>
-      <TextField
-        label="Search by ID"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <Button onClick={handleSearch} variant="contained" color="primary">Search</Button>
-    </div>
+    <Paper style={{padding: 16, margin: '15px 0'}}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={8} sm={9}>
+          <TextField
+            label="Search by ID"
+            fullWidth
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={4} sm={3}>
+          <Button
+            onClick={handleSearch}
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
